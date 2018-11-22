1e11312abf1e3184b62767c4c7130a1ec1559a30
perlancar (on pc-office) <perlancar@gmail.com>
2018-11-22 11:35:40 +0700

    Dump %main::SPEC into x.main.spec

--- lib/Perinci/CmdLine/Base/Patch/DumpAndExit.pm
+++ lib/Perinci/CmdLine/Base/Patch/DumpAndExit.pm
@@ -8,7 +8,7 @@ use strict;
 no warnings;
 
 use Data::Dump;
-use Module::Patch 0.19 qw();
+use Module::Patch qw();
 use base qw(Module::Patch);
 
 our %config;
@@ -28,6 +28,7 @@ sub patch_data {
                 sub_name    => 'run',
                 code        => sub {
                     my $self = shift;
+                    $self->{'x.main.spec'} = \%main::SPEC;
                     _dump($self);
                     $config{-exit_method} eq 'exit' ? exit(0) : die;
                 },
